7ab2b9880af1598dfc5d5007154de479afbf00e4
Grigore Stefan <g_stefan@yahoo.com>
2021-08-09 15:44:07 +0300

    Update

---
+++ build/msvc.version-dependency-update.cmd
@@ -0,0 +1,8 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+echo -^> version-dependency-update quantum-script-extension-openssl
+
+xyo-cc --update-version-dependency quantum-script-extension-openssl
--- source/quantum-script-extension-openssl-version.cpp
+++ source/quantum-script-extension-openssl-version.cpp
@@ -16,9 +16,9 @@ namespace Quantum {
 				namespace Version {
 
 					static const char *version_ = "2.0.0";
-					static const char *build_ = "11";
-					static const char *versionWithBuild_ = "2.0.0.11";
-					static const char *datetime_ = "2021-07-23 16:34:04";
+					static const char *build_ = "12";
+					static const char *versionWithBuild_ = "2.0.0.12";
+					static const char *datetime_ = "2021-08-05 20:38:18";
 
 					const char *version() {
 						return version_;
