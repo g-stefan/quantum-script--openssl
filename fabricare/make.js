c1b09e9711b189b03f162665d55921d51b19fd0b
Grigore Stefan <g_stefan@yahoo.com>
2022-12-13 13:21:53 +0200

    Update

--- fabricare/make.js
+++ fabricare/make.js
@@ -3,6 +3,8 @@
 // SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
+messageAction("make");
+
 exitIf(fileToCS("--touch=source/XYO/QuantumScript.Extension/OpenSSL/License.cpp", "--file-in=source/XYO/QuantumScript.Extension/OpenSSL/License/quantum-script--openssl.txt", "--file-out=source/XYO/QuantumScript.Extension/OpenSSL/License.quantum-script--openssl.Source.cpp", "--is-string-direct"));
 exitIf(fileToCS("--touch=source/XYO/QuantumScript.Extension/OpenSSL/License.cpp", "--file-in=source/XYO/QuantumScript.Extension/OpenSSL/License/bzip2.txt", "--file-out=source/XYO/QuantumScript.Extension/OpenSSL/License.bzip2.Source.cpp", "--is-string-direct"));
 exitIf(fileToCS("--touch=source/XYO/QuantumScript.Extension/OpenSSL/License.cpp", "--file-in=source/XYO/QuantumScript.Extension/OpenSSL/License/libxml2.txt", "--file-out=source/XYO/QuantumScript.Extension/OpenSSL/License.libxml2.Source.cpp", "--is-string-direct"));
@@ -13,7 +15,7 @@ runInPath("source/XYO/QuantumScript.Extension/OpenSSL",function(){
 	exitIf(fileToCS("--touch=Library.cpp","--file-in=Library.js","--file-out=Library.Source.cpp","--is-string","--name=librarySource"));
 });
 
-if (!Fabricare.include("msvc.make." + Project.make)) {
+if (!Fabricare.include("make." + Project.make)) {
 	messageError("Don't know how to make '" + Project.make + "'!");
 	exit(1);
 };
