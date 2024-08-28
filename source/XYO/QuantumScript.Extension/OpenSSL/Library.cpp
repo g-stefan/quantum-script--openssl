f5e767cd121bf48be97c82c4e429bb511139485d
Grigore Stefan <g_stefan@yahoo.com>
2024-08-28 17:05:31 +0300

    Update

--- source/XYO/QuantumScript.Extension/OpenSSL/Library.cpp
+++ source/XYO/QuantumScript.Extension/OpenSSL/Library.cpp
@@ -1202,8 +1202,10 @@ namespace XYO::QuantumScript::Extension::OpenSSL {
 	};
 };
 
-#ifdef XYO_PLATFORM_COMPILE_DYNAMIC_LIBRARY
+#ifndef XYO_QUANTUMSCRIPT_EXTENSION_OPENSSL_LIBRARY
+#	ifdef XYO_PLATFORM_COMPILE_DYNAMIC_LIBRARY
 extern "C" XYO_QUANTUMSCRIPT_EXTENSION_OPENSSL_EXPORT void quantumScriptExtension(XYO::QuantumScript::Executive *executive, void *extensionId) {
 	XYO::QuantumScript::Extension::OpenSSL::initExecutive(executive, extensionId);
 };
+#	endif
 #endif
